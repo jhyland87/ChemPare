00c05435d7bbb0c9e9a4addcd20503d9d8d7b1ac
Justin Hyland <jhyland87@gmail.com>
2025-02-15 22:25:40 -0700

    changes

--- module-test.py
+++ module-test.py
@@ -2,8 +2,11 @@ from suppliers.supplier_3schem import Supplier3SChem
 from suppliers.supplier_laboratoriumdiscounter import SupplierLaboratoriumDiscounter
 
 test_labdiscounter = SupplierLaboratoriumDiscounter('acetone')
-print('LaboratoriumDiscounter Price:',test_labdiscounter.price)
-
+print('LaboratoriumDiscounter')
+print('\tName:',test_labdiscounter.name)
+print('\tPrice:',test_labdiscounter.price)
 
 test_3schem = Supplier3SChem('clean')
-print('3SChem Price:',test_3schem.price)
+print('3SChem')
+print('\tName:',test_3schem.name)
+print('\tPrice:',test_3schem.price)
