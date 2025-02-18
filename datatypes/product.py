96a79cbf16c60d8ceba2133779f1f31df250032b
Justin Hyland <jhyland87@gmail.com>
2025-02-17 20:20:15 -0700

    Adding custom datatype to datatypes folder

---
+++ datatypes/product.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass, astuple
+from typing import List, Set, Tuple, Dict, Any
+
+
+@dataclass
+class Product:
+    """Custom data class for products"""
+
+    # Unique identifier used by supplier
+    uuid: str = None
+
+    # Title of the product
+    title: str = None
+
+    # Product name (sometimes different than title)
+    name: str = None
+
+    # Product description
+    description: str = None
+
+    # URL to direcet product (if availabe)
+    url: str = None
+
+    # CAS Number
+    cas: str = None
+
+    # Price of product
+    price: float = None
+
+    # Currency the price is in
+    currency: str = None
+
+    # Quantity of listing
+    quantity: float = None
+
+    # Unit of measurement for quantity
+    uom: str = None
+
+    def update(self, data):
+        self.__dict__.update(data)
