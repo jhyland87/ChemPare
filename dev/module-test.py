e390850f981728c3e7789041cf13fa26098e1a00
Justin Hyland <jhyland87@gmail.com>
2025-02-18 06:07:14 -0700

    Turning the search factory into an actual class

--- dev/module-test.py
+++ dev/module-test.py
@@ -3,16 +3,18 @@ import os, sys
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-import search_factory
+from search_factory import SearchFactory
 
 query = 'mercury'
 
-results = search_factory.product_search(query)
+print(f'Searching {len(SearchFactory.suppliers)} suppliers for "{query}"...')
+
+product_search = SearchFactory(query)
 
 print('\n\n')
-print(f'RESULTS: - Found {len(results)} results for "{query}:\n')
+print(f'RESULTS: - Found {len(product_search.results)} results for "{query}":\n')
 
-for product in results:
+for product in product_search.results:
     if product.name: print('  Supplier:', product.supplier) 
     if product.name: print('  Name:', product.name) 
     if product.price: print('  Price:', product.price)
--- search_factory.py
+++ search_factory.py
@@ -1,26 +1,58 @@
 import os, sys
+from abcplus import finalmethod
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 import suppliers
 
-def product_search(query):
-    all_results = []
+class SearchFactory(object):
+    # suppliers property lets scripts call 'SearchFactory.suppliers' to get a list of suppliers
+    suppliers = suppliers.__all__
 
-    for supplier in suppliers.__all__:
-        supplier_module = getattr(suppliers, supplier)
-        if __debug__:
-            print(f'Searching for {query} from {supplier_module.__name__}...')
-        res = supplier_module(query, 2)
-        if not res:
-            if __debug__:
-                print('  No results found\n')
-            next
+    __results: list = []
+
+    def __init__(self, query, limit=3):
+        """Init for SearchFactory
         
-        if __debug__:
-            print(f'  found {len(res.products)} products\n')
-        all_results.extend(res.products)
+        Args:
+            query: Search query
+            limit: Amount to limit the search by
+        """
+        
+        self.__query(query, limit)
 
-    return all_results
+    def __query(self, query, limit=None):
+        """Query function (private)
+        
+        Iterates over the suppliers, running the query, then returning the results.
 
+        Args:
+            query: Search query
+            limit: Amount to limit the search by
+
+        Returns:
+            List of Product elements
+        """
+
+        for supplier in suppliers.__all__:
+            supplier_module = getattr(suppliers, supplier)
+            if __debug__:
+                print(f'Searching for {query} from {supplier_module.__name__}...')
+            res = supplier_module(query, limit)
+            if not res:
+                if __debug__:
+                    print('  No results found\n')
+                next
+            
+            if __debug__:
+                print(f'  found {len(res.products)} products\n')
+            self.__results.extend(res.products)
+
+        return self.__results
+    
+    @property
+    @finalmethod 
+    def results(self):
+        """Results getter"""
+        return self.__results
