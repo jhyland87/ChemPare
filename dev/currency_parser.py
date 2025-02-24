81485cac086695410cea12bca31d037f8fd1cc77
J <jhyland87@gmail.com>
2025-02-24 10:57:37 -0700

    Update currency_parser.py
    Breaking up the pattern into multiple lines

--- dev/currency_parser.py
+++ dev/currency_parser.py
@@ -1,7 +1,7 @@
 import regex
 
-pattern = r'^(?:(?P<currency>\p{Sc}|(?:us|au|ca)d?|eu|chf|rub)\s?(?P<price>[0-9]+(?:[,\.][0-9]+)*)|(?P<price>[0-9]+(?:[,\.][0-9]+)*)\s?(?P<currency>\p{Sc}|(?:us|au|ca)d?|eu|chf|rub))$'
-
+pattern = (r'^(?:(?P<currency>\p{Sc}|(?:us|au|ca)d?|eu|chf|rub)\s?(?P<price>[0-9]+(?:[,\.][0-9]+)*)'
+            r'|(?P<price>[0-9]+(?:[,\.][0-9]+)*)\s?(?P<currency>\p{Sc}|(?:us|au|ca)d?|eu|chf|rub))$')
 examples = [
     '56£','$1','1$','$1','1$','₹98,85','55,23₹','$23.43','¥23',
     '$ 23','$ 123,231','£25,''€23.43','€23','€123,231','€ 23.43',
