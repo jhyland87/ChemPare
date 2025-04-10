7f37fa56eb6883caae812053d88bce4be9026045
Justin Hyland <jhyland87@gmail.com>
2025-04-10 10:10:13 -0700

    updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import chempare
 
 def pytest_report_header(config):
     chempare.called_from_test = True
-    ret = ["project deps: chempare-0.1"]
+    ret = ["project deps: chempare-0.0"]
     ret.append(f"chempare.called_from_test: {chempare.called_from_test}")
     if config.getoption("verbose") > 0:
         ret.extend([f"info1: Verbosity: {config.getoption('verbose')}"])
